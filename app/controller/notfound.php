83ebe21cc667e0f7311485c327aa300b44090b25
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-05-04 05:04:28 +0700

    DOCS: uncover code

--- app/controller/notfound.php
+++ app/controller/notfound.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Controller class for throw 404 response code
+ *
+ * @package SemeFramework
+ * @since SemeFramework 1.0
+ * 
+ * @codeCoverageIgnore
+ */
 class NotFound extends SENE_Controller
 {
     public function __construct()
