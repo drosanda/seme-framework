0f78c2a8e023212e6de5f9cbf26494acef5962a6
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-05-05 06:05:35 +0700

    FEAT: add manual sonnar-scanner for sonarcloud.io code coverage

---
+++ kero/bin/scan.sh
@@ -0,0 +1,5 @@
+sonar-scanner \                                                      ✔  10s  06:01:37
+  -Dsonar.organization=drosanda \
+  -Dsonar.projectKey=drosanda_seme-framework \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io
