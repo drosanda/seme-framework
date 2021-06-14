49455e2d88adc1566b23eb074ba62f5789f4881b
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-06-14 17:04:08 +0700

    FIX: wrong sonar

--- kero/bin/scan.sh
+++ kero/bin/scan.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-<<<<<<< HEAD
-=======
-export SONAR_TOKEN="632526e8df83f15f8af7c2f59f317f7b9c5dbde1"
->>>>>>> 49c1ceef4bea3afc7070bbba4b62235b1102142c
-sonar-scanner -Dsonar.organization=drosanda -Dsonar.projectKey=drosanda_apptanya -Dsonar.sources=. -Dsonar.host.url=https://sonarcloud.io
+sonar-scanner -Dsonar.organization=drosanda -Dsonar.projectKey=drosanda_seme-framework -Dsonar.sources=. -Dsonar.host.url=https://sonarcloud.io
