49c1ceef4bea3afc7070bbba4b62235b1102142c
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-06-14 16:57:19 +0700

    FEAT: add scanner and coverall

--- kero/bin/scan.sh
+++ kero/bin/scan.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sonar-scanner -Dsonar.organization=drosanda -Dsonar.projectKey=drosanda_seme-framework -Dsonar.sources=. -Dsonar.host.url=https://sonarcloud.io
+export SONAR_TOKEN="632526e8df83f15f8af7c2f59f317f7b9c5dbde1"
+sonar-scanner -Dsonar.organization=drosanda -Dsonar.projectKey=drosanda_apptanya -Dsonar.sources=. -Dsonar.host.url=https://sonarcloud.io
